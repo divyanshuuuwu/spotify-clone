8d9d975c1020c9602fdb43535b7b611c2e86bad7
divyanshu <divyanshusah44@gmail.com>
2026-08-02 00:19:19 +0530

    regitser api working

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 const userModel = require("../models/user.model")
 const jwt = require("jsonwebtoken")
+const bcrypt = require("bcryptjs")
 
 
 async function registerUser(req,res){
@@ -17,11 +18,37 @@ async function registerUser(req,res){
         })
     }
 
-    await userModel.create({
+    const hashedPassword = await bcrypt.hash(password,10)
+
+    const user = await userModel.create({
         username,
         email,
-        password,
+        password: hashedPassword,
         role
     })
 
+
+    const token = jwt.sign({
+
+        id: user._id,
+        role: user.role
+    },process.env.JWT_SECRET)
+
+
+    res.cookie("token",token)
+
+    res.status(201).json({
+        message:"user registered sucessfully",
+        user:{
+            username:user.username,
+            email:user.email,
+            role:user.role                  
+        }
+    })
+
+
 }
+
+module.exports = {
+    registerUser
+}   
--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
+const authController = require("../controllers/auth.controller.js")
 
-router.post("/register", (req, res) => {
-   
-});
+
+router.post("/register", authController.registerUser)
+
 
 
 
